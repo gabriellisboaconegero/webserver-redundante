e5b579e855da224d06f003f7c8d3f0cbdfc29925
gabriellisboaconegero <gabriellisboaconegero@gmail.com>
2025-01-16 12:18:03 -0300

    Dados de coleta enviados para banco e inicialização da tabela de informações

--- 00_init.sql
+++ 00_init.sql
@@ -1,2 +1,7 @@
-CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'replicator_password';
-SELECT pg_create_physical_replication_slot('replication_slot');
+CREATE TABLE IF NOT EXISTS infos (
+    id SERIAL PRIMARY KEY,
+    uso_cpu float,
+    memoria INT,
+    ip TEXT,
+    time_collect TIMESTAMP default current_timestamp
+);
--- coletor.sh
+++ coletor.sh
@@ -6,18 +6,21 @@
 #   - Uso de memória total
 #   - Endereço de IP
 collect() {
-    cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')
+    cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
 
     memory_info=$(free -m | grep "Mem:")
     memory_total=$(echo $memory_info | awk '{print $2}')
     memory_used=$(echo $memory_info | awk '{print $3}')
     memory_percent=$(echo "scale=2; $memory_used / $memory_total * 100" | bc)
 
-    ip_address=$(hostname -I)
+    ip_address=$(hostname -I | awk '{print $1}')
 
-    echo "CPU Usage (%): $cpu_usage"
-    echo "Memory Usage (%): $memory_percent%"
-    echo "IP Address: $ip_address"
+    source .env
+    QUERY_SQL="insert into infos (uso_cpu, memoria, ip) values ($cpu_usage, $memory_used, '$ip_address')"
+    POSTGRES_URI=postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:9999/postgres
+    echo Executando: $POSTGRES_URI "|" $QUERY_SQL
+
+    psql "${POSTGRES_URI}" -c "${QUERY_SQL}"
 }
 
 # Fica em loop coletando os dados
