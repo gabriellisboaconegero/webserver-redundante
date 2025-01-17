b49a6075be54d2f54160360cbf17831f13c1d7cc
gabriellisboaconegero <gabriellisboaconegero@gmail.com>
2025-01-17 13:57:01 -0300

    Alertando sobre os bancos que estão fora do ar

--- server/app.py
+++ server/app.py
@@ -58,6 +58,7 @@ def index():
     # diversos campos, um deles (campo 7) diz para qual banco foi feito o load balancing
     # da ultima query
     db_name = [info[1] for info in pool_nodes if info[7] == 'true'][0]
+    down_dbs = [info[1] for info in pool_nodes if info[3] == 'down']
 
     conn.close()
 
@@ -77,11 +78,19 @@ def index():
         <p>Timestamp da coleta: {{data[4]}}</p>
         <p>IP do host: {{data[3]}}</p>
         <p>Latência com google.com: {{data[5]}}ms</p>
+        {% if down_dbs | length > 0 %}
+            <h1> Bancos fora do ar </h1>
+            <ul>
+                {% for db in down_dbs %}
+                    <li>{{db}}</li>
+                {% endfor %}
+            </ul>
+        {% endif %}
     </body>
     </html>
     """
 
-    return render_template_string(html_template, host=host_name, data=data, db_name=db_name)
+    return render_template_string(html_template, host=host_name, data=data, db_name=db_name, down_dbs=down_dbs)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
